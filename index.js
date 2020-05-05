08519587c359f3d5815f1785f6c4bf7ae714eb2f
span <spanghosh49@gmail.com>
2020-05-05 12:56:43 +0530

    smscreenecits

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-window.addEventListener("keydown",(event)=>{
-    let span=document.querySelectorAll("span")
-    span.forEach(element => {
-        if(element.innerHTML == event.key.toUpperCase()){
-            playmedia(document.getElementById(event.key.toUpperCase()),element)
-        }
+const span=document.querySelectorAll("span")
+
+if(window.screen.width <= 500){
+    span.forEach(element=>{
+        element.addEventListener("click",(event)=>{
+            playmedia(document.getElementById(event.target.innerHTML),element)
+        })
     })
-})
+}
+else{
+    window.addEventListener("keydown",(event)=>{
+        span.forEach(element => {
+            if(element.innerHTML == event.key.toUpperCase()){
+                playmedia(document.getElementById(event.key.toUpperCase()),element)
+            }
+        })
+    })
+}
 
 function playmedia(element,parentelem){
     element.play()
